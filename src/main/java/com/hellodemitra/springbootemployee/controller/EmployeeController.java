46401965dc7fcdc5297bfb9546061bd7874301f5
demitra <hellodemitra@gmail.com>
2023-09-06 22:16:17 +0300

    Get all employees REST API setup

--- src/main/java/com/hellodemitra/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/hellodemitra/springbootemployee/controller/EmployeeController.java
@@ -4,10 +4,9 @@ import com.hellodemitra.springbootemployee.model.Employee;
 import com.hellodemitra.springbootemployee.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -19,10 +18,20 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    //build create employee REST API
+    // build create employee REST API
     @PostMapping
     public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
         return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
 
     }
+
+    // build get all employees REST API
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+
+
+    }
+
+
 }
--- src/main/java/com/hellodemitra/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/hellodemitra/springbootemployee/service/EmployeeService.java
@@ -2,6 +2,9 @@ package com.hellodemitra.springbootemployee.service;
 
 import com.hellodemitra.springbootemployee.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee saveEmployee(Employee employee);
+    List<Employee> getAllEmployees();
 }
