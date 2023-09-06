4baa9550111d81c14b106054a93f4df1aa3d01af
demitra <hellodemitra@gmail.com>
2023-09-06 21:15:46 +0300

    Initial commit

---
+++ src/main/java/com/hellodemitra/springbootemployee/SpringbootEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.hellodemitra.springbootemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hellodemitra/springbootemployee/SpringbootEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellodemitra.springbootemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
